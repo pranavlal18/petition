f325ba6928cd4716f5dcbb1e65b3f74e0e70edad
Pranav lal <pranavlal18@gmail.com>
2024-10-27 19:08:42 +0530

    just fixes minor issues

--- e_sign_portal/e_sign_portal/settings.py
+++ e_sign_portal/e_sign_portal/settings.py
@@ -42,6 +42,11 @@ INSTALLED_APPS = [
     'complaints'
 ]
 
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Assuming 'static' directory is in the root of your project
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -118,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -127,4 +132,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = BASE_DIR / 'media'
